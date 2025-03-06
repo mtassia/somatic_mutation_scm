4244c1e249053c8f93a62efbacc6992caaebef3d
mtassia <mtassia1@jhu.edu>
2025-03-06 15:19:26 -0500

    Updates to multiSCM reporting; implementing test for singleton mutations

--- dev/function_tests.R
+++ dev/function_tests.R
@@ -1,6 +1,6 @@
 source("dev/bin/SCM_functions.R")
-options(vsc.dev.args = list(width = 1200,height=1000))
-par(mar = c(2,2,2,2))
+options(vsc.dev.args = list(width = 1200, height = 1000))
+par(mar = rep(5, 4))
 
 ##### Data import #####
 #Read tree
@@ -37,7 +37,7 @@ summarise_scm.snp(multiSimmap = scm_example,
                   locus = var,
                   plot = TRUE)
 
-par(mar = c(5,5,5,5))
+par(mar = c(5, 5, 5, 5))
 plot(density(scm_example))
 
 #Plot state priors and posteriors w.r.t. tree
@@ -77,69 +77,69 @@ for (i in sample(names(gt_list.snp), size = 20, replace = FALSE)) {
   tmp <- summary(scm)
 }
 
-##### SCM INDELS #####
-site = sample(names(gt_list.indel),size=1)
-scm_example <- runSCM_single(
-  x = site,
-  tree = tr,
-  gt_state_list = gt_list.indel,
-  Qmat = indel.q,
-  reduced = FALSE,
-  reps = 1000,
-  cores = 6)
-par(mfrow = c(1, 1), ask = FALSE)
-plot_density_hist.indel(scm_example)
-summarise_scm.indel(scm_example, plot = TRUE, title = site)
+# ##### SCM INDELS #####
+# site = sample(names(gt_list.indel),size=1)
+# scm_example <- runSCM_single(
+#   x = site,
+#   tree = tr,
+#   gt_state_list = gt_list.indel,
+#   Qmat = indel.q,
+#   reduced = FALSE,
+#   reps = 1000,
+#   cores = 6)
+# par(mfrow = c(1, 1), ask = FALSE)
+# plot_density_hist.indel(scm_example)
+# summarise_scm.indel(scm_example, plot = TRUE, title = site)
 
-par(mfrow = c(5, 5))
-null <- sapply(scm_example[sample(seq(1, 1000), size = 25, replace = FALSE)],
-            plot,
-            ftype = "off",
-            lwd = 1.5,
-            colors = c("REF" = "gray", "HET" = "royalblue", "ALT" = "tomato"))
+# par(mfrow = c(5, 5))
+# null <- sapply(scm_example[sample(seq(1, 1000), size = 25, replace = FALSE)],
+#             plot,
+#             ftype = "off",
+#             lwd = 1.5,
+#             colors = c("REF" = "gray", "HET" = "royalblue", "ALT" = "tomato"))
 
-par(mfrow = c(4, 5))
-for (i in sample(names(gt_list.indel), size = 20, replace = FALSE)){
-  scm < -runSCM_single(x = i,
-    tree = tr,
-    gt_state_list = gt_list.indel,
-    Qmat = indel.q,
-    reps = 1000,
-    cores = 6,
-    reduced = TRUE)
-  print(paste0("Q logL: ",scm[[1]]$logL[1]))
-  summarise_scm.indel(scm, plot = TRUE, title = i, legend = FALSE)
-  lapply(scm, function(x) {x$logL[1]}) %>% unlist() %>% summary()
-  tmp <- summary(scm)
-}
+# par(mfrow = c(4, 5))
+# for (i in sample(names(gt_list.indel), size = 20, replace = FALSE)){
+#   scm < -runSCM_single(x = i,
+#     tree = tr,
+#     gt_state_list = gt_list.indel,
+#     Qmat = indel.q,
+#     reps = 1000,
+#     cores = 6,
+#     reduced = TRUE)
+#   print(paste0("Q logL: ",scm[[1]]$logL[1]))
+#   summarise_scm.indel(scm, plot = TRUE, title = i, legend = FALSE)
+#   lapply(scm, function(x) {x$logL[1]}) %>% unlist() %>% summary()
+#   tmp <- summary(scm)
+# }
 
-scm_summary <- summary(scm_example)
-Prior_post <- data.frame(gt_list.indel[[site]]) %>%
-  select(Indiv, REF, HET, ALT) %>%
-  pivot_longer(cols=2:4, names_to = "genotype", values_to = "prior") %>%
-  left_join(as.data.frame(scm_summary$tips) %>%
-              rownames_to_column(var = "Indiv") %>%
-              pivot_longer(cols = 2:ncol(.),
-                           names_to = "genotype",
-                           values_to = "posterior")) %>%
-  mutate(posterior = case_when(is.na(posterior) ~ 0,
-                               TRUE ~ posterior))
-Prior_post$genotype <- factor(Prior_post$genotype,
-                              levels = c("REF", "HET", "ALT"))
+# scm_summary <- summary(scm_example)
+# Prior_post <- data.frame(gt_list.indel[[site]]) %>%
+#   select(Indiv, REF, HET, ALT) %>%
+#   pivot_longer(cols=2:4, names_to = "genotype", values_to = "prior") %>%
+#   left_join(as.data.frame(scm_summary$tips) %>%
+#               rownames_to_column(var = "Indiv") %>%
+#               pivot_longer(cols = 2:ncol(.),
+#                            names_to = "genotype",
+#                            values_to = "posterior")) %>%
+#   mutate(posterior = case_when(is.na(posterior) ~ 0,
+#                                TRUE ~ posterior))
+# Prior_post$genotype <- factor(Prior_post$genotype,
+#                               levels = c("REF", "HET", "ALT"))
 
-ggplot(Prior_post, aes(x = prior, y = posterior, fill = genotype)) +
-  geom_point(size = 4, shape = 21) +
-  geom_abline(slope = 1, linetype = 2, color = "gray") +
-  facet_wrap(~genotype) +
-  scale_fill_manual(values = c("REF" = "gray",
-                               "HET" = "royalblue",
-                               "ALT" = "tomato"),
-                    guide = "none")+
-  coord_equal(xlim = c(0, 1), ylim = c(0, 1)) +
-  theme_cowplot() +
-  theme(strip.background = element_blank(),
-        strip.text = element_text(face = "bold"),
-        plot.title = element_text(hjust = 0.5, face = "bold"))
+# ggplot(Prior_post, aes(x = prior, y = posterior, fill = genotype)) +
+#   geom_point(size = 4, shape = 21) +
+#   geom_abline(slope = 1, linetype = 2, color = "gray") +
+#   facet_wrap(~genotype) +
+#   scale_fill_manual(values = c("REF" = "gray",
+#                                "HET" = "royalblue",
+#                                "ALT" = "tomato"),
+#                     guide = "none")+
+#   coord_equal(xlim = c(0, 1), ylim = c(0, 1)) +
+#   theme_cowplot() +
+#   theme(strip.background = element_blank(),
+#         strip.text = element_text(face = "bold"),
+#         plot.title = element_text(hjust = 0.5, face = "bold"))
 
 ##### HDF5 TESTS #####
 source("dev/bin/SCM_functions.R")
@@ -148,13 +148,13 @@ multi_scm(gt_state_list = gt_list.snp,
           tree = tr,
           Q = snp.q,
           scm_its = 100,
-          cores = 6,
+          cores = 16,
           h5f_path = "dev/data/test.h5",
           chr = "all",
           overwrite = FALSE,
           dryrun = TRUE)
 
-h5 <- H5Fopen("dev/data/test_chr21.h5")
+h5 <- H5Fopen("dev/data/test.h5")
 h5_summary <- bind_rows(h5$`summary`) %>%
               tibble()
 events <- lapply(h5$`assigned_edges`, sum) %>%
@@ -186,6 +186,7 @@ ggplot(h5_summary %>%
   labs(x = "95% HPD lower bound",
        y = "95% HPD upper bound",
        fill = "log10(count)")
+
 ggplot(events, aes(x = values)) +
   geom_histogram(binwidth = 1, fill = "lightblue", color = "black") +
   scale_y_log10() +
@@ -193,6 +194,7 @@ ggplot(events, aes(x = values)) +
   theme_cowplot() +
   labs(x = "Mutations assigned to tree (PP ≥ 95%)",
        y = "Count")
+
 h5_summary %>%
   mutate(HPDrange = (muts95high - muts95low) + 1) %>%
   group_by(HPDrange,muts_assigned) %>%
@@ -217,4 +219,88 @@ ggplot(h5_summary,
   theme_cowplot() +
   labs(x = "Assigned mutations (PP ≥ 95%)",
        y = "Q matrix log-likelihood")
+
+##### TESTING PRIOR FOR SIMPLIFYING METHODS #####
+# Use poisson-binomial method to perform a likelihood ratio test on whether a variant
+# has evidence of being a singleton
+
+## Create example data
+var <- sample(names(gt_list.snp), size = 1)
+singleton_lrt(x = var, gt_state_list = gt_list.snp)
+df <- tibble(gt_list.snp[[var]]) %>%
+  select_if(~ !is.numeric(.) || sum(.) != 0)
+  
+
+## Plot example
+df %>%
+  select(!c(locus, Indiv)) %>%
+  mutate(conf = ifelse(pmax(.[[1]], .[[2]], .[[3]]) >= 0.95,
+                       "strong",
+                       "weak")) %>%
+  mutate(prior_gt = colnames(.)[apply(.[, 1:3], 1, which.max)]) %>%
+  group_by(prior_gt, conf) %>%
+  summarise(count = n()) %>%
+  ggplot(aes(x = prior_gt, fill = conf, y = count)) +
+  geom_col(position = position_dodge(preserve = "single")) +
+  geom_hline(yintercept = 1, linetype = 2) +
+  scale_fill_brewer(palette = "Set2") +
+  scale_y_continuous(breaks = scales::pretty_breaks()) +
+  theme_cowplot()
+
+scm_example <- runSCM_single(x = var,
+                             tree = tr,
+                             gt_state_list = gt_list.snp,
+                             Qmat = snp.q,
+                             reduced = FALSE,
+                             reps = 100,
+                             cores = 8)
+
+plot_state_probs_on_tree(locus_str = var,
+                         gt_state_list = gt_list.snp,
+                         scm_summary = summarise_scm.snp(multiSimmap = scm_example,
+                                                         locus = var,
+                                                         plot = FALSE),
+                         tree = tr)
+
+singleton_lrt(x = var, gt_state_list = gt_list.snp)
+
+## If prior consensus state is the het state, perform SCM
+## If the prior consensus state is not the prior, proceed to next step
+
+## Pull states
+states <- df %>%
+  select(!c(locus, Indiv)) %>%
+  colnames()
+
+## Pull consensus state (max of mean priors)
+prior_con <- df %>%
+  select(!c(locus, Indiv)) %>%
+  colMeans() %>%
+  .[which.max(.)] %>%
+  names()
+
+## Pull potential mutant states (given diploid site)
+var_states <- states[states != prior_con]
+
+df[, var_states] %>%
+  rowSums() %>%
+  dpoisbinom(seq(0, nrow(df)), pp = .) %>% 
+  data.frame(x = seq(0, nrow(df)), p = .) %>%
+  ggplot(aes(x = x, y = p)) +
+  geom_vline(xintercept = 1, linetype = 1, color = "skyblue") +
+  geom_col() +
+  scale_x_continuous(breaks = scales::pretty_breaks()) +
+  theme_cowplot()
+
+p_single <- df[, var_states] %>%
+  rowSums() %>%
+  dpoisbinom(1, pp = ., log_d = TRUE)
+
+p_alt <- df[, var_states] %>%
+  rowSums() %>%
+  dpoisbinom(c(0, seq(2, nrow(df))), pp = ., log_d = FALSE) %>%
+  sum(na.rm = TRUE) %>%
+  log10()
+
+pchisq(-2 * (p_single - p_alt), df = 1, lower.tail = FALSE)
 
